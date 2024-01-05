5ac65d52c30c3c0eabcb17fb5ea4a5cf18f98f56
Ranjith V <ranjithkv0708@gmail.com>
2024-01-05 23:17:06 +0530

    Created raw app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { DepartmentModule } from './department/department.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DepartmentModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
